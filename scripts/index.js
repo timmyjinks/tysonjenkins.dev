4b5ab43c5d106c124597d4baae954d6ae6febde5
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-03-03 18:50:52 -0700

    Update home page to get a quote from an api

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,18 @@
 quote = document.querySelector("#hero-quote");
-backgroundImage = document.querySelector('.hero-section');
+backgroundImage = document.querySelector(".hero-section");
 
 images = ["hero-section", "hero-section1", "hero-section2"];
 
 i = 1;
-setInterval(e => {
-    if (i == images.length) {  
-        i = 0;
-    }
-    backgroundImage.classList = [];
-    backgroundImage.classList.add(images[i]);
-    console.log(i);
-    i++;
-}, 5000)
+setInterval((e) => {
+  if (i == images.length) {
+    i = 0;
+  }
+  backgroundImage.classList = [];
+  backgroundImage.classList.add(images[i]);
+  i++;
+}, 5000);
 
-
-// fetch("https://animechan.xyz/api/random/anime?title=naruto")
-//   .then((response) => response.json())
-//   .then((data) => (quote.innerHTML = data.quote));
+fetch("https://animechan.xyz/api/random/anime?title=naruto")
+  .then((response) => response.json())
+  .then((data) => (quote.innerHTML = data.quote));
