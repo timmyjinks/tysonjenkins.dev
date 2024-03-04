61678a3b33c7d39aacfe110b5c9428ff7256a52b
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-03-03 18:28:50 -0700

    Complete the home page

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+quote = document.querySelector("#hero-quote");
+backgroundImage = document.querySelector('.hero-section');
+
+images = ["hero-section", "hero-section1", "hero-section2"];
+
+i = 1;
+setInterval(e => {
+    if (i == images.length) {  
+        i = 0;
+    }
+    backgroundImage.classList = [];
+    backgroundImage.classList.add(images[i]);
+    console.log(i);
+    i++;
+}, 5000)
+
+
+// fetch("https://animechan.xyz/api/random/anime?title=naruto")
+//   .then((response) => response.json())
+//   .then((data) => (quote.innerHTML = data.quote));
